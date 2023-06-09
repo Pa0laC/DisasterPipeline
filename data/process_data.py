5daf367591e5f67f338053037ed4b739340ef6a3
Paola <paola_croll@yahoo.fr>
2023-06-09 16:44:11 +0100

    Running ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,54 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Load data from csv files into dataframes and merge into 1
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = categories.merge(messages, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Create columns for each category, remove duplicates and fill empty values
+    '''
+    # Identify the category names
+    categories = df['categories'].str.split(';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.str[:-2]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+    
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'],axis=1,inplace=True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(keep='first', inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' +str(database_filename))
+    df.to_sql('DisasterTable', engine, index=False)
     pass  
 
 
