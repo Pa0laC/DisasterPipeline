8b6080141f0675470f51b936a61624428b0f7efa
Paola <paola_croll@yahoo.fr>
2023-06-10 13:56:13 +0100

    Adding some code for the training pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,27 +1,109 @@
 import sys
+import pandas as pd
+import pickle
+import re
 
+import nltk
+nltk.download(['punkt','wordnet', 'stopwords'])
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from sqlalchemy import create_engine, inspect, text
+
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import f1_score, classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 def load_data(database_filepath):
-    pass
+    '''
+    Load data from database into dataframe df, and split into features and target for model
+    '''
+    # load data from database
+    engine = create_engine('sqlite:///' + str(database_filepath))
+    with engine.connect() as conn:
+        df= conn.execute(text("SELECT * FROM DisasterTable")).fetchall()
+        inspector = inspect(engine)
+        columns = inspector.get_columns('DisasterTable')
+        column_names = [column['name'] for column in columns]
+        
+    # Create dataframe from loaded data
+    df = pd.DataFrame(df, columns= column_names)
+    
+    # Category names, also target variales
+    category_names = ['related', 'request', 'offer',
+           'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
+           'security', 'military', 'child_alone', 'water', 'food', 'shelter',
+           'clothing', 'money', 'missing_people', 'refugees', 'death', 'other_aid',
+           'infrastructure_related', 'transport', 'buildings', 'electricity',
+           'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
+           'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+           'other_weather', 'direct_report']
+    
+    # Set target variables and feature names
+    X = df['message']
+    Y = df[category_names]
+    
+    return X, Y, category_names
+
 
 
 def tokenize(text):
-    pass
+    '''
+    Process the text data by normalizing it, and removing punctuation
+    '''
+    #Normalize the text and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    words = word_tokenize(text)
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in words if w not in stopwords.words("english")]
+    return lemmed
 
 
 def build_model():
-    pass
+    '''
+    Build model pipeline and split data into training and test dataset
+    '''
+    
+    pipeline = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
+                     ('tfidf', TfidfTransformer()),
+                     ('classifier', MultiOutputClassifier(RandomForestClassifier()))])
+    
+    # Grid search to optimize hyperparameters
+    parameters = parameters = {'classifier__estimator__n_estimators': [50,100],
+                               'classifier__estimator__max_depth': [None, 5],}
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=10)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Evaluate the model by generating a classification report for each category
+    '''
+    y_pred = model.predict(X_test)
+    output = {}
+    for i in range(len(category_names)):
+        output[category_names[i]] =  classification_report(y_pred[i], Y_test.iloc[i])
+    return output
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Save model in pickle file for later use
+    '''
+    # save the model as a pickle file
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
+    '''
+    Generate model
+    '''
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
